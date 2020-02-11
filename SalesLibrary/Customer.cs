5de11d9de3abeff4d502f986e055ca3aa99d9183
max-student <max-student@maxtrain.com>
2020-02-11 11:55:14 -0500

    Add project files.

---
+++ SalesLibrary/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TernaryAccessModifierProject {
+    public class Customer {
+
+        private static int NextId = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        private bool IsNationalAccount { get; set; }
+
+        public override string ToString() {
+            return $"Id ={this.Id}, Name={this.Name}, NAcct={this.IsNationalAccount}";
+        }
+        static Customer() {
+            //read file to get the next id to be assigned
+        }
+        
+
+
+        public void NationalAccount(string YesOrNo) {
+            this.IsNationalAccount = YesOrNo.Equals("yes") ? true : false;
+        }
+        public static string PrintNextId() {
+            return $"Next id is {NextId}";
+        }
+        public Customer(string Name) {
+            this.Id = NextId;
+            this.Name = Name;
+            NextId++;
+        }
+        
+    }
+}
